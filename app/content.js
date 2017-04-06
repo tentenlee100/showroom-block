720799e463c0a55cd6835b2acc5d790e08a1a358
tentenlee100 <tentenlee100@gmail.com>
2017-04-07 01:43:58 +0800

    0.3
    修正停止直播後還是會撥放youtube的bug

--- app/content.js
+++ app/content.js
@@ -1,33 +1,50 @@
-
-
-function stopAutoPlay(){
-  var youtubePlayer = document.getElementById('youtubePlayer');
-  var srcUrl = youtubePlayer.src;
-  srcUrl = srcUrl.replace('autoplay=1&','');
-  youtubePlayer.src = srcUrl;
+function stopAutoPlay() {
+    var youtubePlayer = document.getElementById('youtubePlayer');
+    var srcUrl = youtubePlayer.src;
+    srcUrl = srcUrl.replace('autoplay=1&', '');
+    youtubePlayer.src = srcUrl;
+    youtubePlayer.id = 'youtubePlayer-new'
 }
 
-function remove(){
-  document.getElementById('youtubePlayer').remove();
+function remove() {
+    document.getElementById('youtubePlayer').remove();
 }
-function checkAction(type){
-  if (type === undefined){
-    stopAutoPlay();
-  }else if (type == 'stopAutoPlay'){
-    stopAutoPlay();
-  }else{
-    remove();
-  }
+
+function checkAction(type) {
+    if (type === undefined) {
+        stopAutoPlay();
+    } else if (type == 'stopAutoPlay') {
+        stopAutoPlay();
+    } else {
+        remove();
+    }
 }
 // Read it using the storage API
 chrome.storage.sync.get('type', function(item) {
-  checkAction(item.type);
+    checkAction(item.type);
 });
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  if (changes["type"]) {
-
-    // checkAction(changes["type"].newValue);
-  }
-
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        chrome.storage.sync.get('type', function(item) {
+            checkAction(item.type);
+        });
+    });
 });
+// 參數：決定要監控哪些物件
+var config = {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree:true
+}
+
+// 跟 MutationObserver 指定要監控的目標與參數
+observer.observe(document.getElementById('js-room-section'), config);
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+//   if (changes["type"]) {
+//
+//     // checkAction(changes["type"].newValue);
+//   }
+//
+// });
--- app/popout.js
+++ app/popout.js
@@ -6,35 +6,40 @@
 
 // Read it using the storage API
 chrome.storage.sync.get('type', function(item) {
-  if (item.type === undefined){
-    document.getElementById('stopAutoPlay').checked = true;
-  }else if (item.type == 'stopAutoPlay'){
-    document.getElementById('stopAutoPlay').checked = true;
-  }else{
-    document.getElementById('remove').checked = true;
-  }
+    if (item.type === undefined) {
+        document.getElementById('stopAutoPlay').checked = true;
+    } else if (item.type == 'stopAutoPlay') {
+        document.getElementById('stopAutoPlay').checked = true;
+    } else {
+        document.getElementById('remove').checked = true;
+    }
 });
 
-function reloadAllTab(){
-  chrome.tabs.query({ url: 'https://www.showroom-live.com/*'}, function (tabs) {
+function reloadAllTab() {
+    chrome.tabs.query({
+        url: 'https://www.showroom-live.com/*'
+    }, function(tabs) {
 
-    for (tab in tabs){
-      chrome.tabs.reload();
-    }
- });
+
+        for (var index in tabs) {
+            chrome.tabs.reload(tabs[index].id);
+        }
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('button').onclick = function(){
-    var type = 'stopAutoPlay';
-    if (document.getElementById('stopAutoPlay').checked == true){
-      type = 'stopAutoPlay';
-    }else if (document.getElementById('remove').checked == true) {
-      type = 'remove';
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('button').onclick = function() {
+        var type = 'stopAutoPlay';
+        if (document.getElementById('stopAutoPlay').checked == true) {
+            type = 'stopAutoPlay';
+        } else if (document.getElementById('remove').checked == true) {
+            type = 'remove';
+        }
+        chrome.storage.sync.set({
+            'type': type
+        }, function() {
+            reloadAllTab();
+            window.close();
+        });
     }
-    chrome.storage.sync.set({'type':type}, function() {
-      reloadAllTab();
-      window.close();
-    });
-  }
 });
