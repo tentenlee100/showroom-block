47d85aeb85f4ce904a2e2a762904758bd49eeee7
tentenlee100 <tentenlee100@gmail.com>
2017-04-18 06:32:50 +0800

    0.4

--- app/content.js
+++ app/content.js
@@ -1,13 +1,17 @@
 function stopAutoPlay() {
     var youtubePlayer = document.getElementById('youtubePlayer');
-    var srcUrl = youtubePlayer.src;
-    srcUrl = srcUrl.replace('autoplay=1&', '');
-    youtubePlayer.src = srcUrl;
-    youtubePlayer.id = 'youtubePlayer-new'
+    if (youtubePlayer != null){
+      var srcUrl = youtubePlayer.src;
+      srcUrl = srcUrl.replace('autoplay=1&', '');
+      youtubePlayer.src = srcUrl;
+      youtubePlayer.id = 'youtubePlayer-new'
+    }
 }
 
 function remove() {
+  if (document.getElementById('youtubePlayer') != null){
     document.getElementById('youtubePlayer').remove();
+  }
 }
 
 function checkAction(type) {
@@ -40,7 +44,11 @@ var config = {
 }
 
 // 跟 MutationObserver 指定要監控的目標與參數
-observer.observe(document.getElementById('js-room-section'), config);
+if (document.getElementById('js-room-section') != null){
+  observer.observe(document.getElementById('js-room-section'), config);
+}
+
+
 // chrome.storage.onChanged.addListener(function(changes, namespace) {
 //   if (changes["type"]) {
 //
